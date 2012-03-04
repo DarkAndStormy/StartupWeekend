94cee6af56610d3de6c5a31465f41ee03d3a40c7
Brian P. Holt <bholt@planetholt.com>
2012-03-03 20:10:38 -0600

    spent a lot of time writing a little code

--- test/test-chat-handler.js
+++ test/test-chat-handler.js
@@ -1,29 +1,57 @@
 (function () {
 	'use strict';
 
-	var buster = require('buster'),
+	var mocks = require('mocks'),
+		buster = require('buster'),
 		sinon = require('sinon'),
 		chatHandler = require('../lib/chat-handler'),
-		sockets = {
-			emit: sinon.spy(),
-			broadcast: {
-				emit: sinon.spy()
-			}
-		};
+		socket,
+		chatHandler,
+		chant;
 
 	buster.testCase('ChatHandler', {
 
-		'on connection, we emit an event telling the browser it connected successfully': function () {
-			chatHandler.onConnection(sockets);
-
-			assert.called(sockets.emit);
-			assert.equals(sockets.emit.args[0][0], 'status');
-			assert.equals(sockets.emit.args[0][1], 'connected');
+		setUp: function () {
+			socket = {
+				on: sinon.spy(),
+				emit: sinon.spy(),
+				broadcast: {
+					emit: sinon.spy()
+				}
+			};
 		},
 
-		'// on connection we start listening for messages': function () {
+		'// report bugs to mocks': function () {
+			// loadFile does not have the same relative paths as require
+			// IIFE doesn't work in production code when using loadFile
+		},
 
-		}
+		'on connection, we emit an event telling the browser it connected successfully': function () {
+			chatHandler.onConnection(socket);
+
+			assert.called(socket.emit);
+			assert.equals(socket.emit.args[0][0], 'status');
+			assert.equals(socket.emit.args[0][1], 'connected');
+		},
+
+		'we start listening for messages': function () {
+			chatHandler.onConnection(socket);
+
+			assert.called(socket.on);
+			assert.equals(socket.on.args[0][0], 'message');
+			assert.isFunction(socket.on.args[0][1]);
+		},
+
+		'onMessage emits message as a broadcast': function () {
+			var data = {};
+			chatHandler.onConnection(socket);
+
+			socket.on.args[0][1].call(socket, data);
+
+			assert.called(socket.broadcast.emit);
+			assert.equals(socket.broadcast.emit.args[0][0], 'message');
+			assert.equals(socket.broadcast.emit.args[0][1], data);
+		},
 
 	});
 
